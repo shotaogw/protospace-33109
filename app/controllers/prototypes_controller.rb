e7c36c8fadd6fda44cda6f52d012fa8518bfe513
shotaogw <shotaogw@gmail.com>
2020-12-25 11:02:06 +0900

    initial commit

---
+++ app/controllers/prototypes_controller.rb
@@ -0,0 +1,61 @@
+class PrototypesController < ApplicationController
+  before_action :set_prototype, only: [:edit, :show]
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :move_to_index,  except: [:show, :index]
+  
+  def index
+    @prototypes = Prototype.all
+  end
+
+  def new
+    @prototype = Prototype.new
+  end
+
+  def create
+    if Prototype.create(prototype_params)
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def show
+    @comment = Comment.new
+    @comments = @prototype.comments.includes(:user)
+  end
+
+  def edit
+  end
+
+  def update
+    prototype = Prototype.find(params[:id])
+    if prototype.update(prototype_params)
+      redirect_to prototype_path(prototype)
+    else
+      render action: :edit
+    end
+  end
+
+  def destroy
+    Prototype.find(params[:id]).destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def prototype_params
+    params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
+  end
+
+  def set_prototype
+    @prototype = Prototype.find(params[:id])
+  end
+
+  def move_to_index
+    @prototype = Prototype.find(params[:id])
+    unless current_user.id == @prototype.user_id
+      redirect_to action: :index
+    end
+  end
+
+end
